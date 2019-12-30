d7f9c52b71572ad2465964d17210c992674a7d88
svenkatesh2019 <sridevivenkatesh@outlook.com>
2019-12-30 13:27:02 -0500

    second commit 12/30

--- src/com/edureka/MyCourses/EnrollCourses.java
+++ src/com/edureka/MyCourses/EnrollCourses.java
@@ -1,15 +1,19 @@
 package com.edureka.MyCourses;
 
+import java.util.Iterator;
+import java.util.List;
 import java.util.NoSuchElementException;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.CacheLookup;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import com.edureka.Blogs.EdurekaHome;
@@ -37,7 +41,7 @@ public class EnrollCourses {
 	@CacheLookup
 	WebElement ongoingMycourse;
 	
-	@FindBy(how = How.XPATH,using = "//span[text()='Sridevi Venkates']")
+	@FindBy(how = How.XPATH,using = "//*[@id=\"header-II\"]/section/nav/div/ul[2]/li[1]/a/span")
 	@CacheLookup
 	WebElement user;
 	
@@ -47,6 +51,18 @@ public class EnrollCourses {
 	WebElement logout;
 	
 	
+	@FindBy(how = How.XPATH,using = "//a[text()='All Courses']")
+	@CacheLookup
+	WebElement allCourses;
+	
+	@FindBy(how = How.XPATH,using = "/html/body/section[3]/div[1]/div[1]/div/div/div[1]/div[1]/button")
+	@CacheLookup
+	WebElement jobrole;
+	
+	@FindBy(how = How.XPATH,using = "//button[contains(text(),'Other Filters')]")
+	@CacheLookup
+	WebElement otherFilters;
+	
 	public void checkEnrolledCourses()
 	{
 		//checkMyCourses();
@@ -55,7 +71,72 @@ public class EnrollCourses {
 			logger.info("you have enrolled in a course");
 		else
 			logger.info("you have not enrolled in any course'");
+		checkAllCourses();
+		setJobRoleFilter();
+		selectRoleType();
+		setOtherFilters();
 		logout();
+	}
+
+	private void selectRoleType() {
+		try
+		{
+			WebElement element =driver.findElement(By.xpath("//label[@for='_Test_Automation_Engineer_']"));
+//			JavascriptExecutor js = (JavascriptExecutor) driver;
+//			 js.executeScript("arguments[0].scrollIntoView();", element);
+//			logger.info("scrolled down to view Test Automation engineer ");
+			element.click();
+			logger.info("selected Test Automation Engineer ");
+						
+		}
+		catch(Exception ex)
+		{
+			logger.info("error selecting job role type radio button in 'Job Role' filter");
+			ex.printStackTrace();
+		}
+		
+	}
+
+	private void setOtherFilters() {
+		try
+		{
+			otherFilters.click();
+			logger.info("clicked on 'Other Filters' ");
+		}
+		catch(Exception ex)
+		{
+			logger.info("error clicking on 'Other Filters'  Filter");
+			ex.printStackTrace();
+		}
+	}
+
+	private void setJobRoleFilter() {
+		
+		try
+		{
+			jobrole.click();
+			logger.info("selected 'Job Role '  Filter");
+
+		}
+		catch(Exception ex)
+		{
+			logger.info("error clicking on 'Job Role '  Filter");
+			ex.printStackTrace();
+		}
+		
+	}
+
+	private void checkAllCourses() {
+		try
+		{
+			allCourses.click();
+			logger.info("clicked on 'All Courses' page");
+		}
+		catch(Exception e)
+		{
+			logger.info("error going to 'All Courses' page");
+			e.printStackTrace();
+		}
 	}
 
 	private boolean checkIfOngoingCourses() {
